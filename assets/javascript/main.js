cf009e407810711596577f322955e51e7e062521
randall ishino <rishino24@gmail.com>
2018-01-16 12:31:17 -0800

    yelp API

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,7 +3,11 @@
 var apiKey = "a5c09d2dbe71875e792a7dbe0771b30f&q=";
 var queryURL = "http://food2fork.com/api/search?key=";
 var input = [];
-var limit = "&count=1";
+var count = "&count=1";
+
+var yelpKey = "UekWtwsdSdChv7BPj0qe5Koex2GpQ_eO4MkJJIJYyPst0QClEqU5szdU60hG5Vav-qMNnEATvZBHF2z7txq0P6wSAAafOWzLqT2EJrm0I8yRhXPEDXAON_XWAVVeWnYx"
+var yelpURL = "https://api.yelp.com/v3/businesses/search?key=";
+var limit = "&limit=10";
 
 $("#search").on("click",function(event) {
     event.preventDefault();
@@ -24,21 +28,37 @@ $("#search").on("click",function(event) {
 
 
 function getRecipe() {
-    var newURL = queryURL + apiKey + input + limit;
+    var newURL = queryURL + apiKey + input + count;
 
     $.ajax({
         url:newURL,
-        method: "GET"
+        method: "GET",
+        dataType: "json",
       })
       .done(function(response) {
           console.log(newURL);
           console.log(response);
-          console.log(response.title);
+          console.log(response.recipes[0]);
 
-          var recipeTitle = $("<h1>").text(response.title);
-          var recipeLink = $("<a>").attr("href", response.f2f_url).append(recipeTitle);
-          var recipeImage = $("<img>").attr("src", response.image_url);
+          var recipeTitle = $("<h1>").text(response.recipes[0].title);
+          var recipeLink = $("<a>").attr("href", response.recipes[0].f2f_url).append(recipeTitle);
+          var recipeImage = $("<img>").attr("src", response.recipes[0].image_url);
 
           $("#recipe").append(recipeLink, recipeImage);
       });
+    };
+
+
+    function getRestaurant() {
+        var newURL2 = yelpURL + yelpKey + input + limit;
+
+        $.ajax({
+            url: yelpURL,
+            method: "GET",
+            Authorization: "Bearer<yelpKey>"
+        })
+        .done(function(data) {
+            console.log(data);
+            console.log(newURL2);
+        });
     };
