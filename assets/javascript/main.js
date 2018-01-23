793b3501f40b212b3f31221bbe2d8921537cc0cf
dkwazhere <dhealth77@gmail.com>
2018-01-22 20:10:34 -0800

    create dropdown menu

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -41,7 +41,7 @@ var config = {
       var errorMessage = error.message;
       console.log("Error Message: " + errorMessage);
       console.log("Error code: " + errorCode);
-    
+
       // calling for password/email validation
       userValidation();
 
@@ -49,7 +49,7 @@ var config = {
 });
 
 
-$("#login_btn2").on("click",function(event){ 
+$("#login_btn2").on("click",function(event){
     event.preventDefault();
     userEmail = $("#input_email").val().trim();
     userPassword = $("#input_password").val().trim();
@@ -147,15 +147,14 @@ function getRecipe() {
           var recipe = response.recipes;
           var randomRecipe = recipe[Math.floor(Math.random() * recipe.length)];
           console.log(randomRecipe)
-          for (i=0; i<3; i++) {
-              var recipeTitle = $("<h1>").text(response.recipes[i].title);
-              var recipeLink = $("<a>").attr("href", response.recipes[i].f2f_url).append(recipeTitle);
-              var recipeImage = $("<img>").attr("src", response.recipes[i].image_url);
-              var ingredients = $("<ol>").text(response.recipes[i].ingredients);
-              var recipeFrame = $("<iframe>").attr("src", response.recipes[i].source_url);
-              $("#recipe").append(recipeTitle, recipeLink, recipeImage, ingredients, recipeFrame);
-              console.log(response);
-          }
+          var recipeFrame1 = $("<iframe>").attr("src", response.recipes[1].source_url);
+          var recipeFrame2 = $("<iframe>").attr("src", response.recipes[1].source_url);
+          var recipeFrame3 = $("<iframe>").attr("src", response.recipes[2].source_url);
+          $("#option1").append(recipeFrame1);
+          $("#option2").append(recipeFrame2);
+          $("#option3").append(recipeFrame3);
+          console.log(response);
+          // }
       });
     };
 
@@ -182,6 +181,11 @@ function getZomato() {
     .done(function(response){
         console.log(response);
         console.log(zomatoURL);
+        $("#zomato").prepend(response.restaurants[0].restaurant.name);
+        $('#zomatopic').attr("src", response.restaurants[0].restaurant.photos_url);
+        $('#zomatolocation').prepend(response.restaurants[0].restaurant.location.address + "<br>");
+        $('#zomatolocation').append(response.restaurants[0].restaurant.location.city + "<br>");
+        $('#zomatolocation').append(response.restaurants[0].restaurant.location.zipcode);
     });
 };
 
@@ -191,6 +195,3 @@ function hideButtons() {
     $("#search").hide();
     $("#search-input").hide();
 }
-
-
-   
